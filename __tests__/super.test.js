ca51f7d5bcddf094639f17b206e1d43c5e9f7890
Kai Kohlsdorf <kai.w.kohlsdorf@gmail.com>
2022-06-26 14:34:42 -0700

    Add test 3.

--- __tests__/super.test.js
+++ __tests__/super.test.js
@@ -1,21 +1,27 @@
 import Human from '../src/super.js';
 
 describe('Human', () => {
+
+  let reusableHuman;
+  
+  beforeEach(() => {
+    human = new Human(39, 79);
+  })
+
+  test('should show how beforeEach() works', () => {
+    console.log(human);
+  });
+  
   test('should create an object with age and expectancy properties equal to input', () => {
     const human = new Human (39, 79);
     expect(human.age).toEqual(39);
     expect(human.expectantAge).toEqual(79);
   })
 
-  describe('Human', () => {
-    let reusableHuman;
+  test('should return age on planet Mercury', () => {
+    expect(human.ageMercury()).toEqual(9);
+  })
 
-    beforeEach(() => {
-      reusableHuman = new Human(39, 79);
-    })
-
-    test('should show how beforeEach() works', () => {
-      console.log(reusableHuman);
-    });
-  });
+    
+  
 })
